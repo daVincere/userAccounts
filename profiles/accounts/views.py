fdcbf69122675b6653230853d1119ce0f56690cf
Hardeep <hardeepsinghgaff@gmail.com>
2017-05-09 18:43:48 +0530

    second commit

--- profiles/accounts/views.py
+++ profiles/accounts/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+def home(request):
+	if request.user.is_authenticated():
+		context = {
+		'text' : request.user.username
+		}
+	else:
+		context = {
+		'text' : 'get a user logged in',
+		}
+	return render(request, "home.html", context)
